6b3a201ab9d1676622e42ecf30cff554b15d9f20
Jerry Howard <jerryhoward@fastmail.fm>
2019-07-12 19:30:58 +0000

    Integrated 'new' and 'create' actions for portfolio items

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -4,4 +4,28 @@ class PortfolioItemsController < ApplicationController
     @portfolio_items = PortfolioItem.all
   end
 
+  def new
+    @portfolio_item = PortfolioItem.new
+  end
+
+  def create
+    @portfolio_item = PortfolioItem.new(blog_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolio_items_path, notice: 'Portfolio item was successfully created.' }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def blog_params
+      params.require(:portfolio_item).permit(:title, :subtitle, :body)
+    end
+
 end
