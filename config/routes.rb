579206e529e5b2fd48df53a8b201d11ffd3e3240
Jerry Howard <jerryhoward@fastmail.fm>
2019-07-15 23:09:53 +0000

    Customized portfolio_items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :portfolio_items
+  resources :portfolio_items, except: [:show]
+  get 'portfolio/:id', to: 'portfolio_items#show', as: 'portfolio_show'
+  # ^^ In this way we can override the default resources value,
+  # and also add our own custom route method (the last param, as: 'portfolio_show')
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
