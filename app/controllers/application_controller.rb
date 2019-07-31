ad2fb02bf6dffc8da427c0e5c79947cebecf70f7
Jerry Howard <jerryhoward@fastmail.fm>
2019-07-31 21:46:13 +0000

    Implements a simple copyright renderer in the ApplicationController. It will ultimately be moved into a Gem, but for now this is fully working code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = TechnophobicViewTool::Renderer.copyright('Jerry Howard', 'All rights reserved')
+  end
+end
+
+module TechnophobicViewTool
+  class Renderer
+    def self.copyright(name, msg)
+      "&copy; #{Time.now.year} <b>#{name}</b> - #{msg}".html_safe
+    end
+  end
 end
